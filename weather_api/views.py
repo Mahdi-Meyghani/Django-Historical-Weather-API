62ae656c2d809d88997ca831b84703e4c960bf35
Mahdi <mahdimeyghani02@gmail.com>
2024-09-23 17:25:33 +0330

    Almost complete documentation page

--- weather_api/views.py
+++ weather_api/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+import pandas as pd
 
 def documentation(request):
-    return render(request, 'documentation.html')
+    df = pd.read_csv('data/stations.txt', skiprows=17)
+
+    return render(request, 'documentation.html',
+                  context={'stations': df.to_html(index=False, col_space=130)})
