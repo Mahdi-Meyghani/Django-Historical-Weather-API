6d47cb8bcf6735b3a36a74d2b7f1858aa4a8fd28
Mahdi <mahdimeyghani02@gmail.com>
2024-09-23 16:30:08 +0330

    Add data

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('weather_api.urls.py'))
+    path('', include('weather_api.urls.py')),
 ]
