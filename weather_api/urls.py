fe75157a9f3fcb5f029f3adf6391944e18c85a2b
Mahdi <mahdimeyghani02@gmail.com>
2024-09-24 13:21:19 +0330

    Modify weather_api/urls.py

--- weather_api/urls.py
+++ weather_api/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.documentation, name='documentation'),
     path('v1/api/<str:staid>/', views.station, name='station'),
     path('v1/api/<str:staid>/<str:date>/', views.station_date, name='station_date'),
-    path('v1/api/yearly/<str:staid>/<str:year>/', views.station_year, name='station_year')
+    path('v1/api/yearly/<str:staid>/<str:year>/', views.station_year, name='station_year'),
 ]
