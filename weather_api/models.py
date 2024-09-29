69b5aebc9dfc88404d85a89a2dbfaad5c055a578
Mahdi <mahdimeyghani02@gmail.com>
2024-09-29 18:46:30 +0330

    Create the model

--- weather_api/models.py
+++ weather_api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class WeatherData(models.Model):
+    STAID = models.IntegerField()
+    SOUID = models.IntegerField()
+    DATE = models.CharField(max_length=200)
+    TG = models.FloatField()
+    Q_TG = models.IntegerField()
+
+    class Meta:
+        db_table = "weather_data"
