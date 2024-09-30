5b0667298806bc424f7bf8b0ace5d87fa1918ccc
Mahdi <mahdimeyghani02@gmail.com>
2024-09-30 16:20:37 +0330

    Load data in the model

---
+++ weather_api/management/commands/insert_data.py
@@ -0,0 +1,40 @@
+from django.core.management.base import BaseCommand
+from weather_api.models import WeatherData
+import glob
+import pandas as pd
+from pathlib import Path
+
+
+class Command(BaseCommand):
+    help = "Load historical weather data to postgres database"
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write("Start the function handle...")
+        self.stdout.write("======================================================")
+
+        filepaths = glob.glob("data/*.txt")[1:]
+        filepaths = [Path(filepath) for filepath in filepaths]
+
+        for filepath in filepaths:
+            self.stdout.write("Create dataframe of each path")
+            self.stdout.write("======================================================")
+
+            df = pd.read_csv(filepath, skiprows=20)
+            df.columns = map(str.strip, df.columns)
+            df['DATE'] = pd.to_datetime(df['DATE'], format='%Y%m%d')
+            df['DATE'] = df['DATE'].astype(str)
+            df['TG'] = df['TG'] / 10
+
+            for index, row in df.iterrows():
+                self.stdout.write("Start insert that df in model")
+                self.stdout.write("======================================================")
+
+                weatherdata = WeatherData(
+                    STAID=row['STAID'], SOUID=row['SOUID'], DATE=row['DATE'], TG=row['TG'], Q_TG=row['Q_TG']
+                )
+                weatherdata.save()
+
+                self.stdout.write("Finish insert that df in model")
+                self.stdout.write("======================================================")
+
+        print("We are Done man :)")
