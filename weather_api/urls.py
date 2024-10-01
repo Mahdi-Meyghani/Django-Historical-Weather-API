391aaa30fb2da5acb512a3a61586382c36dfd26f
Mahdi <mahdimeyghani02@gmail.com>
2024-10-01 17:51:23 +0330

    Modify app urls (receive integer for station id(staid))

--- weather_api/urls.py
+++ weather_api/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.documentation, name='documentation'),
-    path('v1/api/<str:staid>/', views.station, name='station'),
-    path('v1/api/<str:staid>/<str:date>/', views.station_date, name='station_date'),
-    path('v1/api/yearly/<str:staid>/<str:year>/', views.station_year, name='station_year'),
+    path('v1/api/<int:staid>/', views.station, name='station'),
+    path('v1/api/<int:staid>/<str:date>/', views.station_date, name='station_date'),
+    path('v1/api/yearly/<int:staid>/<str:year>/', views.station_year, name='station_year'),
 ]
